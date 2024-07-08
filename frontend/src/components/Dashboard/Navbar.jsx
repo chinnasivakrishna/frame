482029bd601bda00bc429d030b21a2a7ba55fac5
chinnasivakrishna <139040548+chinnasivakrishna@users.noreply.github.com>
2024-07-08 17:25:13 +0530

    Update Navbar.jsx

--- frontend/src/components/Dashboard/Navbar.jsx
+++ frontend/src/components/Dashboard/Navbar.jsx
@@ -15,7 +15,7 @@ export default function SimpleSlider() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:8001/user/fetch');
+        const response = await axios.get('https://frame-api-five.vercel.app/user/fetch');
         setData(response.data.fetch);
         console.log(response.data.fetch);
       } catch (error) {
